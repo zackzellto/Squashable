88af87bf59e76c1afcf1629f880292fed71eef93
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-05-02 20:16:47 -0600

    Enabled CORS in Program.cs

--- Squashable/Program.cs
+++ Squashable/Program.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Squashable;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo { Title = "Squashable API", Version = "v1", Description = "Squashable API" });
+});
 
 builder.Services.AddControllersWithViews();
 
@@ -14,9 +20,29 @@ builder.Services.AddDbContext<BugDataContext>(options =>
 
 var app = builder.Build();
 
+//Enable CORS
+app.UseCors(options => options
+    .WithOrigins(new[] { "http://localhost:3000", "http://localhost:8080", "http://localhost:4200" })
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    );
+
+
+app.UseSwagger(options =>
+{
+    options.SerializeAsV2 = true;
+});
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Squashable API V1");
+    c.RoutePrefix = string.Empty;
+});
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
