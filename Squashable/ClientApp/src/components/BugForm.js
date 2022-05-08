b384d48133d09b8451176e7e2279a03307d95af8
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-05-08 00:08:21 -0600

    Adding more styling to the buginfo form

--- Squashable/ClientApp/src/components/BugForm.js
+++ Squashable/ClientApp/src/components/BugForm.js
@@ -7,57 +7,139 @@ import {
   Container,
   FloatingLabel,
 } from "react-bootstrap";
+import "./BugForm.css";
+import moment from "moment";
+import { ThemeProvider } from "react-bootstrap";
 
 export function BugForm() {
+  const BugDate = moment().format("LLL");
+
   return (
-    <Container>
-      <Form>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Control type="email" placeholder="Enter email" />
+    <ThemeProvider
+      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+    >
+      <Container id="bugform-container">
+        <Form>
+          <Row md id="buginfo-row1">
+            <Col>
+              <Form.Control
+                type="text"
+                id="bug-id"
+                placeholder="Bug ID"
+                aria-label="Bud ID Tracker"
+                size="sm"
+                disabled
+                readOnly
+              />
+            </Col>
+            <Col md id="bug-date">
+              <Form.Control
+                type="text"
+                value={BugDate}
+                aria-label="Bug Date"
+                size="sm"
+                disabled
+                readOnly
+              ></Form.Control>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col md>
+              <div id="bug-title-container">
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Bug Title"
+                  className="mb-3"
+                >
+                  <Form.Control id="bug-title-form" type="textarea" />
+                </FloatingLabel>
+              </div>
+            </Col>
+            <Col md>
+              <div id="bug-reporter-container">
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Reported By"
+                  className="mb-3"
+                >
+                  <Form.Control id="bug-reporter-form" type="textarea" />
+                </FloatingLabel>
+              </div>
+            </Col>
+          </Row>
+          <div id="bug-description-container">
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Bug Description"
+              className="mb-3"
+            >
+              <Form.Control id="bug-description-form" type="textarea" />
+            </FloatingLabel>
+          </div>
+          <div id="bug-status-container">
+            <FloatingLabel controlId="floatingSelectGrid" label="Bug Status">
+              <Form.Select
+                id="bug-select-menu"
+                aria-label="Bug Status select menu"
+              >
+                <option value=""></option>
+                <option value="1">New</option>
+                <option value="2">In Progress</option>
+                <option value="3">Squashed!</option>
+              </Form.Select>
+            </FloatingLabel>
+          </div>
+          <Row className="mb-3">
+            <Col md>
+              <div id="bug-priority-container">
+                <FloatingLabel
+                  controlId="floatingSelectGrid"
+                  label="Bug Priority"
+                >
+                  <Form.Select
+                    id="bug-select-menu"
+                    aria-label="Bug Priority select menu"
+                  >
+                    <option value=""></option>
+                    <option value="1">High</option>
+                    <option value="2">Mid</option>
+                    <option value="3">Low</option>
+                  </Form.Select>
+                </FloatingLabel>
+              </div>
+            </Col>
+            <Col md>
+              <div id="bug-severity-container">
+                <FloatingLabel
+                  controlId="floatingSelectGrid"
+                  label="Bug Severity"
+                >
+                  <Form.Select
+                    id="bug-select-menu"
+                    aria-label="Bug Severity select menu"
+                  >
+                    <option value=""></option>
+                    <option value="1"></option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                </FloatingLabel>
+              </div>
+            </Col>
+            <Form.Group as={Col} controlId="formGridZip">
+              <Form.Control />
+            </Form.Group>
+          </Row>
+
+          <Form.Group className="mb-3" id="formGridCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-        </Row>
-        <FloatingLabel controlId="floatingTextarea2" label="Comments">
-          <Form.Control
-            as="textarea"
-            placeholder="Leave a comment here"
-            style={{ height: "100px" }}
-          />
-        </FloatingLabel>
-
-        <Form.Group className="mb-3" controlId="formGridAddress2">
-          <Form.Control placeholder="Apartment, studio, or floor" />
-        </Form.Group>
-
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Control />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Select defaultValue="Choose...">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Control />
-          </Form.Group>
-        </Row>
-
-        <Form.Group className="mb-3" id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Container>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </ThemeProvider>
   );
 }
