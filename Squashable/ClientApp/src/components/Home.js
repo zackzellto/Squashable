7906db4613a227254d2950437983d6c752e5456f
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-05-12 14:17:15 -0600

    Adding restriction to Dashboard and Bug Tracker if not logged in

--- Squashable/ClientApp/src/components/Home.js
+++ Squashable/ClientApp/src/components/Home.js
@@ -1,43 +1,72 @@
 import React from "react";
-import { Image, Button, Container } from "react-bootstrap";
-import LoginButton from "../components/Login";
 import { useAuth0 } from "@auth0/auth0-react";
 import DeviceMockups from "../media/device-mockups.png";
 import "./Home.css";
 import "../App.css";
 
-export function Home() {
-  return (
-    <>
-      <div fluid className="home-container">
-        <div className="home-section">
-          <div className="landing-page-heading">
-            <h2>Welcome to</h2>
-            <h1>Squashable.</h1>
-            <h2>a fully functional bug tracking system.</h2>
-            <h3>
-              built for <u className="developer-word">Developers</u>, by{" "}
-              <u className="developer-word">Developers</u>.
-            </h3>
+export const Home = () => {
+  const { isAuthenticated } = useAuth0();
+  const renderHomepage = () => {
+    if (isAuthenticated) {
+      return (
+        <div fluid className="home-container">
+          <div className="home-section">
+            <div className="landing-page-heading">
+              <h2>Welcome to</h2>
+              <h1>Squashable.</h1>
+              <h2>a fully functional bug tracking system.</h2>
+              <h3>
+                built for <u className="developer-word">Developers</u>, by{" "}
+                <u className="developer-word">Developers</u>.
+              </h3>
+              <button className="dashboard-button" to="/dashboard">
+                Dashboard
+              </button>
+            </div>
+            <img
+              id="device-mockups"
+              src={DeviceMockups}
+              class="img-fluid fade-in-image"
+              alt="device-mockups"
+            ></img>
           </div>
-          <img
-            id="device-mockups"
-            src={DeviceMockups}
-            class="img-fluid fade-in-image"
-            alt="device-mockups"
-          ></img>
-
-          {/* <div>
-          <Image className="squashable-logo" src={SquashableLogo}></Image>
-        </div> */}
+          <div className="about-section">
+            <h1>About</h1>
+          </div>
+          <div className="contact-section">
+            <h1>Contact</h1>
+          </div>
         </div>
-        <div className="about-section">
-          <h1>About</h1>
+      );
+    } else {
+      return (
+        <div fluid className="home-container">
+          <div className="home-section">
+            <div className="landing-page-heading">
+              <h2>Welcome to</h2>
+              <h1>Squashable.</h1>
+              <h2>a fully functional bug tracking system.</h2>
+              <h3>
+                built for <u className="developer-word">Developers</u>, by{" "}
+                <u className="developer-word">Developers</u>.
+              </h3>
+            </div>
+            <img
+              id="device-mockups"
+              src={DeviceMockups}
+              class="img-fluid fade-in-image"
+              alt="device-mockups"
+            ></img>
+          </div>
+          <div className="about-section">
+            <h1>About</h1>
+          </div>
+          <div className="contact-section">
+            <h1>Contact</h1>
+          </div>
         </div>
-        <div className="contact-section">
-          <h1>Contact</h1>
-        </div>
-      </div>
-    </>
-  );
-}
+      );
+    }
+  };
+  return <>{renderHomepage()}</>;
+};
--- Squashable/ClientApp/src/components/NavMenu.js
+++ Squashable/ClientApp/src/components/NavMenu.js
@@ -1,40 +1,70 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import Profile from "../components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import "./NavMenu.css";
 
 const NavMenu = () => {
-  return (
-    <Navbar sticky="top" className="navbar" expand="lg">
-      <Container className="navbar-container">
-        <Navbar.Brand variant="light" tag={Link} to="/">
-          Squashable
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto nav-links">
-            <Profile />
+  const { isAuthenticated } = useAuth0();
 
-            <Nav.Link
-              className="nav-about-btn"
-              variant="light"
-              tag={Link}
-              to="/about"
-            >
-              About
-            </Nav.Link>
-            <Nav.Link className="nav-dashboard-btn" tag={Link} to="/dashboard">
-              Dashboard
-            </Nav.Link>
-            <Nav.Link className="nav-contact-btn" tag={Link} to="/contact">
-              Contact
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  if (isAuthenticated) {
+    return (
+      <Navbar sticky="top" className="navbar" expand="lg">
+        <Container className="navbar-container">
+          <Navbar.Brand variant="light" tag={Link} to="/">
+            Squashable
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto nav-links">
+              <Profile />
+
+              <Nav.Link
+                className="nav-about-btn"
+                variant="light"
+                tag={Link}
+                to="/about"
+              >
+                About
+              </Nav.Link>
+              <Nav.Link className="nav-contact-btn" tag={Link} to="/contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  } else {
+    return (
+      <Navbar sticky="top" className="navbar" expand="lg">
+        <Container className="navbar-container">
+          <Navbar.Brand variant="light" tag={Link} to="/">
+            Squashable
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto nav-links">
+              <Profile />
+
+              <Nav.Link
+                className="nav-about-btn"
+                variant="light"
+                tag={Link}
+                to="/about"
+              >
+                About
+              </Nav.Link>
+              <Nav.Link className="nav-contact-btn" tag={Link} to="/contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 };
 
 export default NavMenu;
