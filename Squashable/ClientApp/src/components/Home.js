52eca67676608f28390032980cb6ce0bcce23bb3
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-05-11 19:55:27 -0600

    Added about and contact sections

--- Squashable/ClientApp/src/components/Home.js
+++ Squashable/ClientApp/src/components/Home.js
@@ -6,29 +6,37 @@ import "../App.css";
 
 export function Home() {
   return (
-    <div fluid className="home-container">
-      <div className="landing-page-heading">
-        <h2>Welcome to</h2>
-        <h1>Squashable.</h1>
-        <h2>a fully functional bug tracking system.</h2>
-        <h3>
-          built for <u className="developer-word">Developers</u>, by{" "}
-          <u className="developer-word">Developers</u>.
-        </h3>
-      </div>
-      <Container className="mockup-container">
-        <Image fluid className="device-mockups" src={DeviceMockups}></Image>
+    <>
+      <div fluid className="home-container">
+        <div className="landing-page-heading">
+          <h2>Welcome to</h2>
+          <h1>Squashable.</h1>
+          <h2>a fully functional bug tracking system.</h2>
+          <h3>
+            built for <u className="developer-word">Developers</u>, by{" "}
+            <u className="developer-word">Developers</u>.
+          </h3>
+        </div>
+        <Container className="mockup-container">
+          <Image fluid className="device-mockups" src={DeviceMockups}></Image>
 
-        {/* <div>
+          {/* <div>
           <Image className="squashable-logo" src={SquashableLogo}></Image>
         </div> */}
-      </Container>
-      <Button variant="primary" href="/bug-form">
-        Login
-      </Button>
-      <Button className="" variant="secondary" href="/login">
-        Register
-      </Button>
-    </div>
+        </Container>
+        <Button variant="primary" href="/bug-form">
+          Login
+        </Button>
+        <Button className="" variant="secondary" href="/login">
+          Register
+        </Button>
+        <div className="about-section">
+          <h1>About</h1>
+        </div>
+        <div className="contact-section">
+          <h1>Contact</h1>
+        </div>
+      </div>
+    </>
   );
 }
