7b02c5dd368220167588b6610c06920755f17c50
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-05-17 10:10:38 -0600

    Added search functionality to table

--- Squashable/ClientApp/src/components/BugTable.js
+++ Squashable/ClientApp/src/components/BugTable.js
@@ -1,82 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./BugTable.css";
 import { Button } from "react-bootstrap";
+import axios from "axios";
+
+const API_URL = "https://localhost:7091/api/BugInfo";
 
 function BugTable() {
+  const [bugData, setBugData] = useState([]);
+  const [searchBug, setSearchBug] = useState("");
+
+  const getBugData = async () => {
+    try {
+      const data = await axios.get(API_URL, {});
+      console.log(data.data);
+      setBugData(data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getBugData();
+  }, []);
+
   return (
-    <table className="table table-hover bug-table">
-      <thead>
-        <tr>
-          <th>Bug ID</th>
-          <th>Title</th>
-          <th>Date</th>
-          <th>Update</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Bug 1</td>
-          <td>01/01/2020</td>{" "}
-          <td>
-            <Button>
-              <i class="fa fa-pencil"></i>
-            </Button>
-          </td>
-          <td>
-            <Button>
-              <i class="fa fa-trash"></i>
-            </Button>
-          </td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Bug 2</td>
-          <td>02/01/2020</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Bug 3</td>
-          <td>02/01/2020</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>Bug 4</td>
-          <td>02/01/2020</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>Bug 5</td>
-          <td>02/01/2020</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>Bug 6</td>
-          <td>02/01/2020</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>Bug 7</td>
-          <td>02/01/2020</td>
-        </tr>
-        <tr>
-          <td>8</td>
-          <td>Bug 8</td>
-          <td>02/01/2020</td>
-        </tr>
-        <tr>
-          <td>9</td>
-          <td>Bug 9</td>
-          <td>02/01/2020</td>
-        </tr>
-        <tr>
-          <td>10</td>
-          <td>Bug 10</td>
-          <td>02/01/2020</td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <input
+        type="text"
+        placeholder="Search Bug"
+        onChange={(e) => {
+          setSearchBug(e.target.value);
+        }}
+      ></input>
+      <table className="table table-hover bug-table">
+        <thead>
+          <tr>
+            <th>Bug ID</th>
+            <th>Title</th>
+            <th>Date</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bugData
+            .filter((bug) => {
+              if (searchBug == "") {
+                return bug;
+              } else if (
+                bug.title.toLowerCase().includes(searchBug.toLowerCase())
+              ) {
+                return bug;
+              }
+            })
+            .map((bug) => {
+              return (
+                <tr>
+                  <td>{bug.id}</td>
+                  <td>{bug.title}</td>
+                  <td>{bug.createdDate}</td>
+                  <td>
+                    <Button className="fa fa-pencil update-bg"></Button>
+                  </td>
+                  <td>
+                    <Button className="fa fa-trash delete-bg"></Button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
   );
 }
 
